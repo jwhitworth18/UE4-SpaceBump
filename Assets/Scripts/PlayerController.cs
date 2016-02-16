10286977e6b5cffe0bd714ce5a3d8073d88a48cc
Joseph Whitworth <jwhitworth@fullsail.edu>
2016-02-16 17:20:23 -0500

    Initial File Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveForce = 5;
+    public float boostMulti = 2;
+    Rigidbody2D myBody;
+
+	void Start ()
+    {
+        myBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        Vector2 moveVec = new Vector2();
+	}
+}
